dd4df1209c4b17c3525b608d193252093a3e3530
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-02-29 08:54:58 +0100

    added error component

---
+++ JokesApp/Layout/ErrorMessage.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace JokesApp.Layout;
+
+public partial class ErrorMessage
+{
+    [Parameter]
+    public string Message { get; set; } = "An error occurred";
+
+    [Parameter]
+    public Action? OnRetry { get; set; }
+
+    private void handleRetry()
+    {
+        if (OnRetry != null)
+        {
+            OnRetry?.Invoke();
+        }
+    }
+}
